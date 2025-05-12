a7707452aacf8ca2ae6b532bcac0a39d9db0bce0
Chen-Hsuan Lin <chenhsuanl@nvidia.com>
2025-05-12 09:38:55 -0700

    hotfix offload network (#94)

--- cosmos_predict1/diffusion/inference/world_interpolator.py
+++ cosmos_predict1/diffusion/inference/world_interpolator.py
@@ -29,10 +29,11 @@ import argparse
 import os
 
 import torch
+from megatron.core import parallel_state
 
 from cosmos_predict1.diffusion.inference.inference_utils import add_common_arguments, check_input_frames, validate_args
 from cosmos_predict1.diffusion.inference.world_generation_pipeline import DiffusionWorldInterpolatorGenerationPipeline
-from cosmos_predict1.utils import log, misc
+from cosmos_predict1.utils import distributed, log, misc
 from cosmos_predict1.utils.io import read_prompts_from_file, save_video
 
 torch.enable_grad(False)
@@ -131,13 +132,8 @@ def demo(args):
     validate_args(args, inference_type)
 
     if args.num_gpus > 1:
-        from megatron.core import parallel_state
-
-        from cosmos_predict1.utils import distributed
-
         distributed.init()
         parallel_state.initialize_model_parallel(context_parallel_size=args.num_gpus)
-        process_group = parallel_state.get_context_parallel_group()
 
     # Initialize video_interpolator generation model pipeline
     pipeline = DiffusionWorldInterpolatorGenerationPipeline(
@@ -161,9 +157,6 @@ def demo(args):
         num_frame_pairs=args.num_frame_pairs,
         frame_stride=args.frame_stride,
     )
-
-    if args.num_gpus > 1:
-        pipeline.model.net.enable_context_parallel(process_group)
 
     # Handle multiple prompts if prompt file is provided
     if args.batch_input_path:
